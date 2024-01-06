33e0adc3e232580b8e0db3e0acd3268b43b7f59a
paoloverasteguiflorenttini <paoloverasteguiflorenttini@paolos-mbp>
2024-01-05 20:59:20 -0500

    commited

--- src/demo/TestDemo.java
+++ src/demo/TestDemo.java
@@ -6,9 +6,11 @@ public class TestDemo {
 	public static void main(String[] args) {
 		
 		System.out.println("Friday");
-		System.out.println("im sick");
 		
-		//stashing....
+		
+		System.out.println("Today is not Tuesday");
+		
+		//stashing......
 	}
 	//This is coming from gitHub
 	//Today is Friday
